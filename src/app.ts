8293bb18753b149141edb42a56d4df84bb6e5870
季长春 <jichangchun@jichangchundeMacBook-Pro.local>
2025-03-26 16:07:30 +0800

    feat: 允许跨域

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from 'express';
+import cors from 'cors';
 
 import userRoutes from './models/staff/routes';
 import shopRoutes from './models/shop/shop.routes'; 
@@ -13,6 +14,8 @@ const app = express();
 
 // 解析 JSON 请求体
 app.use(express.json());
+// 允许跨域
+app.use(cors());
 
 // 注册路由
 app.use('/backstage/staff', userRoutes); // 用户模块路由
@@ -22,8 +25,11 @@ app.use('/backstage/departments', departmentRoutes)
 app.use('/backstage/commodity', commodityRouter); // 商品路由
 app.use('/backstage/performance', PerformanceRouter);
 app.use('/backstage/departmentPerformance', DepartmentPerformanceRouter);
-app.use('/', (req: Request, res: Response) => {
-  res.send('Hello word')
+app.use('/*', (req: Request, res: Response) => {
+  res.status(404).send({
+    code: 404,
+    message: '404'
+  })
 })
 // 连接数据库
 connectDB();
